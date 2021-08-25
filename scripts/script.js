a0bbc56f9a37ec4cec6f1cbe6bf6a4e5d3b6ab91
Geff <>
2021-08-25 02:55:51 +0200

    breadcrumbs and active tab preview

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,2 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function test(item) {
-  document.getElementById(`dropdown_menu_${item}`).classList.toggle('show');
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.top_menu_button')) {
-    var dropdowns = document.getElementsByClassName('dropdown_items');
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
+//Temporary set a tab active for rendering preview
+document.getElementById('activeTab').style.backgroundColor = '#1E1E1E';
